9f7e48a8356d5fbe474c7e50001ecaa5efcebebc
skullzado <46558157+skullzado@users.noreply.github.com>
2022-09-26 19:51:52 +0800

    Merge webpack_config to main
    * Initial webpack config
    
    * Add babel config to webpack
    
    * Add eslint and prettier support
    
    * Add typescript support

---
+++ webpack.common.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './js/bundle.js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Project - Weather App',
+      template: './src/template.html',
+      favicon: './src/assets/favicon.ico',
+    }),
+    new MiniCssExtractPlugin({
+      linkType: 'text/css',
+      filename: './css/main.css',
+    }),
+    new ESLintPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        type: 'asset/resource',
+        test: /\.(png|jpe?g|gif|ico)$/i,
+        generator: {
+          filename: 'media/[name][hash][ext]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.json'],
+  },
+};
