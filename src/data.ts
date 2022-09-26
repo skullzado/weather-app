e288c384eee9d4d6c043a2aaa3fd08f670b7c299
Paul Calzado <pafcalzado@gmail.com>
2022-09-26 19:30:30 +0800

    Add typescript support

---
+++ src/data.ts
@@ -0,0 +1,24 @@
+import RainyImgSrc from './assets/anna-atkins-rainy.jpg';
+import CloudyImgSrc from './assets/daria-nepriakhina-cloudy.jpg';
+import ClearImgSrc from './assets/yuriy-bogdanov-clear.jpg';
+
+export const imageSrcObj = {
+  rainy: {
+    src: RainyImgSrc,
+    alt: 'Anna Atkins from Unsplash',
+  },
+  cloudy: {
+    src: CloudyImgSrc,
+    alt: 'Daria Nepriakhina from Unsplash',
+  },
+  clear: {
+    src: ClearImgSrc,
+    alt: 'Yuriy Bogdanov from Unsplash',
+  },
+};
+
+const data = {
+  imageSrcObj,
+};
+
+export default data;
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './style.css';
-import { imageSrcObj } from './data';
-
-const bgImg = document.getElementById('bg-img');
-
-bgImg.src = imageSrcObj.cloudy.src;
-bgImg.alt = imageSrcObj.cloudy.alt;
